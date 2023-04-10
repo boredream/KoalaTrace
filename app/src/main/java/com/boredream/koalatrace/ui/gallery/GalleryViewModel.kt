aab6c24b3db72a490247046657ae91eff5d4c50b
boredream <48262906@qq.com>
2023-04-10 16:49:12 +0800

    init

---
+++ app/src/main/java/com/boredream/koalatrace/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.boredream.koalatrace.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
