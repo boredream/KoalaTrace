cda113aaa77c0221bf18d8d79879c17f486e1aae
boredream <48262906@qq.com>
2023-04-11 17:12:51 +0800

    正常使用

--- app/src/main/java/com/boredream/koalatrace/ui/MainActivity.kt
+++ app/src/main/java/com/boredream/koalatrace/ui/MainActivity.kt
@@ -4,19 +4,20 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.boredream.koalatrace.R
+import com.boredream.koalatrace.base.BaseActivity
 import com.boredream.koalatrace.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     companion object {
         fun start(context: Context) {
@@ -25,21 +26,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun getLayoutId() = R.layout.activity_main
+
+    override fun getViewModelClass() = MainViewModel::class.java
+
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -47,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_mine
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -64,4 +61,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
 }
---
+++ app/src/main/java/com/boredream/koalatrace/ui/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.boredream.koalatrace.ui
+
+import com.boredream.koalatrace.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+class MainViewModel : BaseViewModel() {
+
+}
